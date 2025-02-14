39c68f25722329662f17deda3f3399741259f59e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-02-14 23:08:59 +0800

    main.go: add main package for load config from file.

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/For-ACGN/DeepBot"
+	"github.com/pelletier/go-toml/v2"
+)
+
+var cfgPath string
+
+func init() {
+	flag.StringVar(&cfgPath, "cfg", "config.toml", "set the configuration file path")
+	flag.Parse()
+}
+
+func main() {
+	data, err := os.ReadFile(cfgPath)
+	checkError(err)
+
+	var config deepbot.Config
+	err = toml.Unmarshal(data, &config)
+	checkError(err)
+
+	bot := deepbot.NewDeepBot(&config)
+	bot.Run()
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
