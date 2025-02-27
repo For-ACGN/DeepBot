087623c4d679304a9d83d6e822992bbbdc519265
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-02-27 15:50:14 +0800

    replace bot.replyMessage to bot.replyResponse, fix bug about WriteFile.

--- character.go
+++ character.go
@@ -36,7 +36,7 @@ func (bot *DeepBot) onListCharacter(ctx *zero.Ctx) {
 		list = "当前人设列表: " + list
 	}
 
-	bot.replyMessage(ctx, list)
+	bot.replyResponse(ctx, list)
 }
 
 func (bot *DeepBot) onCurCharacter(ctx *zero.Ctx) {
@@ -56,7 +56,7 @@ func (bot *DeepBot) onCurCharacter(ctx *zero.Ctx) {
 		char = "当前人设: " + char
 	}
 
-	bot.replyMessage(ctx, char)
+	bot.replyResponse(ctx, char)
 }
 
 func (bot *DeepBot) onGetCharacter(ctx *zero.Ctx) {
@@ -64,12 +64,12 @@ func (bot *DeepBot) onGetCharacter(ctx *zero.Ctx) {
 
 	msg := textToArgN(ctx.MessageString(), 2)
 	if len(msg) != 2 {
-		bot.replyMessage(ctx, "非法参数格式")
+		bot.replyResponse(ctx, "非法参数格式")
 		return
 	}
 	name := msg[1]
 	if name == " " || name == "" {
-		bot.replyMessage(ctx, "非法参数格式")
+		bot.replyResponse(ctx, "非法参数格式")
 		return
 	}
 
@@ -77,7 +77,7 @@ func (bot *DeepBot) onGetCharacter(ctx *zero.Ctx) {
 	content, err := os.ReadFile(file)
 	if err != nil {
 		log.Printf("failed to read character file: %s\n", err)
-		bot.replyMessage(ctx, "人设不存在")
+		bot.replyResponse(ctx, "人设不存在")
 		return
 	}
 
@@ -86,22 +86,22 @@ func (bot *DeepBot) onGetCharacter(ctx *zero.Ctx) {
 		char = "当前人设内容为空"
 	}
 
-	bot.replyMessage(ctx, char)
+	bot.replyResponse(ctx, char)
 }
 
 func (bot *DeepBot) onClrCharacter(ctx *zero.Ctx) {
 	user := bot.getUser(ctx.Event.UserID)
 
 	file := fmt.Sprintf("data/characters/%d/current.cfg", user.id)
-	err := os.WriteFile(file, nil, 0644)
+	err := os.WriteFile(file, nil, 0600)
 	if err != nil {
 		log.Printf("failed to update character config: %s\n", err)
 		return
 	}
 
-	user.setCharacter("")
+	user.setCharacter("", "")
 
-	bot.replyMessage(ctx, "清除人设成功")
+	bot.replyResponse(ctx, "清除人设成功")
 }
 
 func (bot *DeepBot) onSetCharacter(ctx *zero.Ctx) {
@@ -109,12 +109,12 @@ func (bot *DeepBot) onSetCharacter(ctx *zero.Ctx) {
 
 	msg := textToArgN(ctx.MessageString(), 2)
 	if len(msg) != 2 {
-		bot.replyMessage(ctx, "非法参数格式")
+		bot.replyResponse(ctx, "非法参数格式")
 		return
 	}
 	name := msg[1]
 	if name == " " || name == "" {
-		bot.replyMessage(ctx, "非法参数格式")
+		bot.replyResponse(ctx, "非法参数格式")
 		return
 	}
 
@@ -122,19 +122,19 @@ func (bot *DeepBot) onSetCharacter(ctx *zero.Ctx) {
 	content, err := os.ReadFile(file)
 	if err != nil {
 		log.Printf("failed to read character file: %s\n", err)
-		bot.replyMessage(ctx, "人设不存在")
+		bot.replyResponse(ctx, "人设不存在")
 		return
 	}
 	file = fmt.Sprintf("data/characters/%d/current.cfg", user.id)
-	err = os.WriteFile(file, []byte(name), 0644)
+	err = os.WriteFile(file, []byte(name), 0600)
 	if err != nil {
 		log.Printf("failed to update character config: %s\n", err)
 		return
 	}
 
-	user.setCharacter(string(content))
+	user.setCharacter(name, string(content))
 
-	bot.replyMessage(ctx, "选择人设成功")
+	bot.replyResponse(ctx, "选择人设成功")
 }
 
 func (bot *DeepBot) onAddCharacter(ctx *zero.Ctx) {
@@ -142,33 +142,33 @@ func (bot *DeepBot) onAddCharacter(ctx *zero.Ctx) {
 
 	msg := textToArgN(ctx.MessageString(), 3)
 	if len(msg) != 3 {
-		bot.replyMessage(ctx, "非法参数格式")
+		bot.replyResponse(ctx, "非法参数格式")
 		return
 	}
 	name := msg[1]
 	if name == " " || name == "" {
-		bot.replyMessage(ctx, "非法参数格式")
+		bot.replyResponse(ctx, "非法参数格式")
 		return
 	}
 	if len(name) > 30 {
-		bot.replyMessage(ctx, "人设名称太长")
+		bot.replyResponse(ctx, "人设名称太长")
 		return
 	}
 
 	content := msg[2]
 	if content == " " || content == "" {
-		bot.replyMessage(ctx, "人设内容为空")
+		bot.replyResponse(ctx, "人设内容为空")
 		return
 	}
 
 	file := fmt.Sprintf("data/characters/%d/%s.txt", user.id, name)
-	err := os.WriteFile(file, []byte(content), 0644)
+	err := os.WriteFile(file, []byte(content), 0600)
 	if err != nil {
 		log.Printf("failed to save character file: %s\n", err)
 		return
 	}
 
-	bot.replyMessage(ctx, "添加人设成功")
+	bot.replyResponse(ctx, "添加人设成功")
 }
 
 func (bot *DeepBot) onDelCharacter(ctx *zero.Ctx) {
@@ -176,12 +176,12 @@ func (bot *DeepBot) onDelCharacter(ctx *zero.Ctx) {
 
 	msg := textToArgN(ctx.MessageString(), 2)
 	if len(msg) != 2 {
-		bot.replyMessage(ctx, "非法参数格式")
+		bot.replyResponse(ctx, "非法参数格式")
 		return
 	}
 	name := msg[1]
 	if name == " " || name == "" {
-		bot.replyMessage(ctx, "非法参数格式")
+		bot.replyResponse(ctx, "非法参数格式")
 		return
 	}
 
@@ -189,7 +189,7 @@ func (bot *DeepBot) onDelCharacter(ctx *zero.Ctx) {
 	err := os.Remove(file)
 	if err != nil {
 		log.Printf("failed to remove character file: %s\n", err)
-		bot.replyMessage(ctx, "人设不存在")
+		bot.replyResponse(ctx, "人设不存在")
 		return
 	}
 
@@ -200,12 +200,12 @@ func (bot *DeepBot) onDelCharacter(ctx *zero.Ctx) {
 		return
 	}
 	if string(char) == name {
-		err = os.WriteFile(file, nil, 0644)
+		err = os.WriteFile(file, nil, 0600)
 		if err != nil {
 			log.Printf("failed to update character config: %s\n", err)
 			return
 		}
 	}
 
-	bot.replyMessage(ctx, "删除人设成功")
+	bot.replyResponse(ctx, "删除人设成功")
 }
