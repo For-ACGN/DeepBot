924aa0b221fcfa6d2212d132e1c4346e0478710e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-03 15:56:35 +0800

    add TestOnSearchWeb and TestOnSearchImage, use os env for set key.

--- func_call_test.go
+++ func_call_test.go
@@ -3,12 +3,41 @@ package deepbot
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/chromedp/chromedp"
 	"github.com/stretchr/testify/require"
 )
+
+func TestOnSearchWeb(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	cfg := &searchCfg{
+		EngineID: os.Getenv("GS_ENGINE_ID"),
+		APIKey:   os.Getenv("GS_API_KEY"),
+		ProxyURL: os.Getenv("HTTP_PROXY"),
+	}
+	output, err := onSearchWeb(ctx, cfg, "Golang")
+	require.NoError(t, err)
+	fmt.Println(output)
+}
+
+func TestOnSearchImage(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	cfg := &searchCfg{
+		EngineID: os.Getenv("GS_ENGINE_ID"),
+		APIKey:   os.Getenv("GS_API_KEY"),
+		ProxyURL: os.Getenv("HTTP_PROXY"),
+	}
+	output, err := onSearchImage(ctx, cfg, "Golang", "medium")
+	require.NoError(t, err)
+	fmt.Println(output)
+}
 
 func TestOnBrowseURL(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
