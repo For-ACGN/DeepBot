8096686bfe599b87a6a744cb04e67f25d7e0d72b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-13 22:55:14 +0800

    add onSetCharacter for set prompt template, update onGetCharacter.

--- character.go
+++ character.go
@@ -81,12 +81,19 @@ func (bot *DeepBot) onGetCharacter(ctx *zero.Ctx) {
 		return
 	}
 
-	char := string(content)
-	if char == "" {
-		char = "当前人设内容为空"
+	output := string(content)
+	if output == "" {
+		output = "当前人设内容为空"
 	}
 
-	bot.sendText(ctx, char)
+	file = fmt.Sprintf("data/characters/%d/%s.tpl", user.id, name)
+	prompt, err := os.ReadFile(file)
+	if err == nil && string(prompt) != "" {
+		output += "\n================prompt================\n"
+		output += string(prompt)
+	}
+
+	bot.sendText(ctx, output)
 }
 
 func (bot *DeepBot) onClrCharacter(ctx *zero.Ctx) {
@@ -99,12 +106,12 @@ func (bot *DeepBot) onClrCharacter(ctx *zero.Ctx) {
 		return
 	}
 
-	user.setCharacter("", "")
+	user.setCharacter("", "", "")
 
 	bot.sendText(ctx, "清除人设成功")
 }
 
-func (bot *DeepBot) onSetCharacter(ctx *zero.Ctx) {
+func (bot *DeepBot) onSelectCharacter(ctx *zero.Ctx) {
 	user := bot.getUser(ctx.Event.UserID)
 
 	args := textToArgN(ctx.MessageString(), 2)
@@ -125,6 +132,9 @@ func (bot *DeepBot) onSetCharacter(ctx *zero.Ctx) {
 		bot.sendText(ctx, "人设不存在")
 		return
 	}
+	file = fmt.Sprintf("data/characters/%d/%s.tpl", user.id, name)
+	prompt, _ := os.ReadFile(file)
+
 	file = fmt.Sprintf("data/characters/%d/current.cfg", user.id)
 	err = os.WriteFile(file, []byte(name), 0600)
 	if err != nil {
@@ -132,9 +142,42 @@ func (bot *DeepBot) onSetCharacter(ctx *zero.Ctx) {
 		return
 	}
 
-	user.setCharacter(name, string(content))
+	user.setCharacter(name, string(content), string(prompt))
 
 	bot.sendText(ctx, "选择人设成功")
+}
+
+func (bot *DeepBot) onSetCharacter(ctx *zero.Ctx) {
+	user := bot.getUser(ctx.Event.UserID)
+
+	args := textToArgN(ctx.MessageString(), 3)
+	if len(args) != 3 {
+		bot.sendText(ctx, "非法参数格式")
+		return
+	}
+	name := args[1]
+	if name == " " || name == "" {
+		bot.sendText(ctx, "非法参数格式")
+		return
+	}
+	if len(name) > 30 {
+		bot.sendText(ctx, "人设名称太长")
+		return
+	}
+	prompt := args[2]
+	if prompt == " " || prompt == "" {
+		bot.sendText(ctx, "提示词模板为空")
+		return
+	}
+
+	file := fmt.Sprintf("data/characters/%d/%s.tpl", user.id, name)
+	err := os.WriteFile(file, []byte(prompt), 0600)
+	if err != nil {
+		log.Printf("failed to save character prompt file: %s\n", err)
+		return
+	}
+
+	bot.sendText(ctx, "添加提示词模板成功")
 }
 
 func (bot *DeepBot) onAddCharacter(ctx *zero.Ctx) {
@@ -192,6 +235,9 @@ func (bot *DeepBot) onDelCharacter(ctx *zero.Ctx) {
 		bot.sendText(ctx, "人设不存在")
 		return
 	}
+
+	file = fmt.Sprintf("data/characters/%d/%s.tpl", user.id, name)
+	_ = os.Remove(file)
 
 	file = fmt.Sprintf("data/characters/%d/current.cfg", user.id)
 	char, err := os.ReadFile(file)
