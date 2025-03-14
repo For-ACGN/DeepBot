5f6316ffd5ecd4176112d397cb243a2e8d032d5c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-14 23:23:52 +0800

    rename status.go to seek.go, move code about update mood to new file.

--- seek.go
+++ seek.go
@@ -4,73 +4,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"log"
 	"strings"
 	"time"
 
 	"github.com/cohesion-org/deepseek-go"
-	"github.com/wdvxdr1123/ZeroBot"
 )
-
-const promptGetMood = `
-根据上下文的对话，请选择以下最合适的心情/情绪/状态，注意只需要回答两个字，请暂时忽略之前已经要求的回答格式。
-可选项: 厌恶、困惑、嫉妒、害羞、尴尬、平静、快乐、恐惧、悲伤、惊讶、愤怒、期待、温暖、感动、生气。
-`
-
-var validMoods = map[string]struct{}{
-	"厌恶": {}, "困惑": {}, "嫉妒": {}, "害羞": {},
-	"尴尬": {}, "平静": {}, "快乐": {}, "恐惧": {},
-	"悲伤": {}, "惊讶": {}, "愤怒": {}, "期待": {},
-	"温暖": {}, "感动": {}, "生气": {},
-}
-
-func isValidMood(mood string) bool {
-	_, ok := validMoods[mood]
-	return ok
-}
-
-func (bot *DeepBot) onGetMood(ctx *zero.Ctx) {
-	user := bot.getUser(ctx.Event.UserID)
-
-	mood := user.getMood()
-	if mood == "" {
-		mood = "平静"
-	}
-
-	bot.sendText(ctx, mood)
-}
-
-func (bot *DeepBot) onUpdateMood(ctx *zero.Ctx) {
-	user := bot.getUser(ctx.Event.UserID)
-
-	mood, err := bot.updateMood(user)
-	if err != nil {
-		log.Printf("failed to update mood: %s\n", err)
-		bot.sendText(ctx, "更新心情失败")
-		return
-	}
-
-	bot.sendText(ctx, mood)
-}
-
-func (bot *DeepBot) updateMood(user *user) (string, error) {
-	req := &ChatRequest{
-		Model:       deepseek.DeepSeekChat,
-		Temperature: 1,
-		TopP:        1,
-		MaxTokens:   8192,
-	}
-	resp, err := bot.seek(req, user, promptGetMood)
-	if err != nil {
-		return "", fmt.Errorf("failed to get mood: %s", err)
-	}
-	mood := resp.Answer
-	if !isValidMood(mood) {
-		return "", fmt.Errorf("get invalid mood: %s", mood)
-	}
-	user.setMood(mood)
-	return mood, nil
-}
 
 // seek is same as chat, but it will not append response after call.
 func (bot *DeepBot) seek(req *ChatRequest, user *user, msg string) (*chatResp, error) {
